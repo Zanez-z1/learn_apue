807223b07b887dd839c32f5b15faa20d690ffac4
wksgl <wksglzz@gmail.com>
2026-09-14 19:34:28 +0800

    2026/9/14

---
+++ apue/stdio/adv/epoll/relay.c
@@ -0,0 +1,233 @@
+#include <asm-generic/errno-base.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/epoll.h>
+
+#define tty1     "/dev/tty11"
+#define tty2     "/dev/tty12"
+#define BUFSIZE  1024
+
+enum 
+{
+    STATE_W,
+    STATE_R,
+STATE_AUTO,
+    STATE_T,
+    STATE_E,
+};
+
+struct fsm_st
+{
+    int state;
+    int sfd;
+    int dfd;
+    int len;
+    int pos;
+    char buf[BUFSIZE];
+    char *errstr;
+};
+
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+static void fsm_driver(struct fsm_st *fsm)
+{
+    int ret;
+    /*开始逐个判断每个状态*/
+    switch (fsm->state)
+    {
+        case STATE_R:
+            /*将读取到的长度存储在结构体里，方便write时使用*/
+            fsm->len = read(fsm->sfd, fsm->buf, BUFSIZE);
+            /*len等于零，代表读取结束，进入STATE_T结束状态*/
+            if (fsm->len == 0)
+            {
+                fsm->state = STATE_T;
+            }
+            /*否则如果len小于零，则需要分情况讨论，如果errno是EAGAIN，代表本次没读到数据，重新开始读
+                * 否则进入STATE_E错误状态*/
+            else if (fsm->len < 0)
+            {
+                if (errno == EAGAIN)
+                {
+                    fsm->state = STATE_R;
+                }
+                else
+                {
+                    fsm->errstr = "read()";
+                    fsm->state = STATE_E;
+                }
+            }
+            /*如果len大于零，说明读到了数据，初始化pos为零，并且将状态切换为STATE_W写状态*/
+            else
+            {
+                fsm->pos = 0;
+                fsm->state = STATE_W;
+            }
+            break;
+
+        case STATE_W:
+            /*state为STATE_W,进入写状态*/
+            ret = write(fsm->dfd, fsm->buf + fsm->pos, fsm->len);
+            /*此处错误判断同read，分假错和真错，分别进行处理*/
+            if (ret < 0)
+            {
+                if (errno == EAGAIN)
+                {
+                    fsm->state = STATE_W;
+                }
+                else
+                {
+                    fsm->errstr = "write()";
+                    fsm->state = STATE_E;
+                }
+
+            }
+            /*ret大于等于0，计算写入了多少字节，如果一次没写完，则重新进入写状态继续写*/
+            else 
+            {
+                fsm->pos += ret;
+                fsm->len -= ret;
+                if (fsm->len > 0)
+                {
+                    fsm->state = STATE_W;
+                }
+                else
+                {
+                    fsm->state = STATE_R;
+                }
+            }
+            break;
+
+        case STATE_E:
+            perror(fsm->errstr);
+            fsm->state = STATE_T;
+            break;
+
+        case STATE_T:
+            break;
+
+        default:
+            abort();
+            break;
+    }
+}
+
+static void relay(int fd1, int fd2)
+{
+    int fd1_save, fd2_save, epollfd; 
+    struct fsm_st fsm12, fsm21;
+    struct epoll_event ev;
+    
+    fd1_save = fcntl(fd1, F_GETFL);
+    fcntl(fd1, F_SETFL, fd1_save | O_NONBLOCK);
+
+    fd2_save = fcntl(fd2, F_GETFL);
+    fcntl(fd2, F_SETFL, fd2_save | O_NONBLOCK);
+
+    fsm12.state = STATE_R;
+    fsm12.sfd = fd1;
+    fsm12.dfd = fd2;
+
+    fsm21.state = STATE_R;
+    fsm21.sfd = fd2;
+    fsm21.dfd = fd1;
+
+    epollfd = epoll_create(1);
+    if (epollfd < 0)
+    {
+        perror("epoll_create()");
+        exit(1);
+    }
+
+    ev.events = 0;
+    ev.data.fd = fd1;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd1, &ev);
+
+    ev.events = 0;
+    ev.data.fd = fd2;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd2, &ev);
+
+    while (fsm12.state != STATE_T || fsm21.state != STATE_T)
+    {
+        /*设置监听事件*/
+        ev.events = 0;
+        ev.data.fd = fd1;
+        
+        if (fsm12.state == STATE_R)
+            ev.events |= EPOLLIN; 
+        if (fsm21.state == STATE_W)
+            ev.events |= EPOLLOUT;
+        epoll_ctl(epollfd, EPOLL_CTL_MOD, fd1, &ev);
+        
+        ev.events = 0;
+        ev.data.fd = fd2;
+        if (fsm12.state == STATE_W)
+            ev.events |= EPOLLOUT; 
+        if (fsm21.state == STATE_R)
+            ev.events |= EPOLLIN;
+        /*开始监听*/
+        if (fsm12.state < STATE_AUTO || fsm21.state < STATE_AUTO)
+        {
+            while (epoll_wait(epollfd, &ev, 1, -1) < 0)
+            {
+                if (errno == EINTR)
+                    continue;
+
+                perror("poll()");
+                exit(1);
+            }
+
+        }
+        /*查看结果*/
+        if (ev.data.fd == fd1 && ev.events & EPOLLIN ||ev.data.fd == fd2 && ev.events &EPOLLOUT || fsm12.state > STATE_AUTO)
+        {
+            fsm_driver(&fsm12);
+        }
+        if (ev.data.fd == fd2 && ev.events & EPOLLIN || ev.data.fd == fd1 & ev.events & EPOLLOUT || fsm21.state > STATE_AUTO)
+        {
+            fsm_driver(&fsm21);
+        }
+    }
+    
+    fcntl(fd1, F_SETFL, fd1_save);
+    fcntl(fd2, F_SETFL, fd2_save);
+    close(epollfd);
+}
+
+int main()
+{
+    int fd1,fd2;
+
+    fd1 = open(tty1, O_RDWR);
+    if (fd1 < 0)
+    {
+        perror("open");
+        exit(1);
+    }
+    write(fd1, "TTY1\n", 5);
+
+    fd2 = open(tty2, O_RDWR | O_NONBLOCK);
+    if (fd2 < 0)
+    {
+        perror("open");
+        exit(1);
+    }
+    write(fd2, "TTY2\n", 5);
+
+    relay(fd1, fd2);
+
+    close(fd2);
+    close(fd1);
+
+    exit(0);
+}
